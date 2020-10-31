a67201418044ab1aaf7c0a22e3851577436a3f25
KimJihun-1315 <rlawlgns1315@gmail.com>
2020-11-01 01:21:08 +0900

    student_control(error fix)

--- Data_Structures/Student_control/main.c
+++ Data_Structures/Student_control/main.c
@@ -1,41 +1,10 @@
-//성적 관리 프로그램
-//학생 번호 순으로 배열에 보관
-//scanf(), scanf_s
-
-//[정보 입력 부분]
-//학생정보를 입력해주세요.
-//학번:
-//이름:
-//주소:
-//전화번호:
-//국어 점수 :
-//자료구조 점수 :
-//
-//[검색 부분]
-//학번을 입력하세요. 1101을 입력하면
-//학생정보(학번, 이름, 주소 등)이 나온다.
-//만약 해당 학생이 없으면 없다고 출력한다.
-//
-//[삭제 기능]
-//전학 등으로 학생이 없을 경우 학생을 삭제하는 기능을 추가한다.
-//해당 학번은 모두 빈 칸으로 출력되게 하되 이름에는 ‘전학’이라는 글자가 나오게 한다.
-//
-//[1등 출력 기능]
-//국어 점수가 가장 높은 학생을 출력하는 기능 추가하기
-//자료구조 점수가 가장 높은 학생을 출력하는 기능 추가하기
-//
-//[공통사항]
-//잘못된 입력이 발생될 경우 판단하여 에러메시지 보여주기.
-//예) 학번을 입력하세요. 11129
-//4자리를 입력하세요.등으로 적절히 처리하기
-
-
 #include<stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define STUDENT_CNT 200
 
+//학생의 정보를 저장할 구조체
 typedef struct studentInfo {
     int num;
     char name[10];
@@ -45,7 +14,7 @@ typedef struct studentInfo {
     int dsScore;
 }studentInfo;
 
-
+//구조체와 성적고득점자를 저장할 공간을 선언
 studentInfo studentsInfo[STUDENT_CNT];
 int hiDSScore = 0;
 int hiKoScore = 0;
@@ -59,15 +28,16 @@ void menu();
 void SelectTopDSScore();
 void SelectTopKoScore();
 
+//프로그램을 실행시켰을때 메뉴(함수)로 이동.
 int main() {
     while(1) menu();
 }
 
+//메뉴로 이동되었을때 출력과 숫자입력을 통해 탭이동.
 void menu() {
     int tapNum;
     char exit_scan;
-    printf("\n");
-    printf("  ====== 학생 관리 프로그램 =======\n");
+    printf("\n  ====== 학생 관리 프로그램 =======\n");
     printf("  +-------------------------------+\n");
     printf("  |                               |\n");
     printf("  |   1.학생관리     2. 학생검색  |\n");
@@ -75,67 +45,66 @@ void menu() {
     printf("  |                               |\n");
     printf("  +-------------------------------+\n");
     printf("  탭이동 : ");
-    scanf("%d", &tapNum);
-
-    printf("\n\n\n");
+    scanf("%d", &tapNum);   //탭넘버를 입력받음
+    system("cls");          //시스템화면 클리어
 
     switch (tapNum)
     {
-    case 1:
-        printf("  ====== 학생 관리 프로그램 =======\n");
+    case 1: //1입력시 학생관리 탭으로 이동
+        printf("\n  ====== 학생 관리 프로그램 =======\n");
         printf("  +--------- 학생 관리 -----------+\n");
         printf("  |                               |\n");
-        printf("  |   1. 학생추가   2. 학생삭제   |\n");
+        printf("  |   1. 학생추가   2. 전학처리   |\n");
         printf("  |                               |\n");
-        printf("  +-------------------------------|\n");
+        printf("  +-------------------------------+\n");
         printf("  탭이동 : ");
         tapNum = 0;
         scanf("%d", &tapNum);
-        printf("\n\n");
+        system("cls");
 
-        if (tapNum == 1) {
+        if (tapNum == 1) {          //1입력시 학생을 추가하는 탭으로 이동
             init();
             return;
         }
-        else if (tapNum == 2) {
+        else if (tapNum == 2) {     //2입력시 전학처리 탭으로 이동
             del();
             return;
         }
-        else {
-            printf("보기중 하나를 선택하시오. \n");
+        else {                      //이외의 경우 ERROR처리
+            printf("\n   ERROR : 보기중 하나를 선택하시오. \n");
             return;
         }
         break;
-    case 2:
+    case 2:         //2입력시 검색창으로 이동
         search();
         break;
-    case 3:
+    case 3:         //3입력시 고득점자 창으로 이동
         topScore();
         break;
-    case 4:
-        printf("종료하시겠습니까?(Y) : ");
+    case 4:         //종료를 확인함
+        printf("\n   종료하시겠습니까?(Y) : ");
         do {
             scanf("%c", &exit_scan);
         } while (exit_scan == 10);
-        if (exit_scan == 'Y')
-            exit(1);
+        if (exit_scan == 'Y' || exit_scan == 'y')   //만약 입력값이 y라면
+            exit(1);            //exit을 이용하여 코드를 종료
         else {
-            printf("종료를 하실려면 Y를 눌러주세요.\n");
+            printf("\n   종료를 하실려면 Y를 눌러주세요.\n");
             return;
         }
         break;
-    default:
-        printf("보기중 하나를 선택하시오. \n");
+    default:       //1,2,3,4외의 숫자를 입력시 ERRO출력
+        printf("\n   ERROR : 보기중 하나를 선택하시오. \n");
         return;
     }
     return;
 }
 
-void init() {
+void init() {       //학생을 추가하여 주는 탭
     char scanNum[5];
     char idx;
-    printf("  ------------학생 추가------------\n");
-    printf("    학번   : ");
+    printf("\n   ------------학생 추가------------\n");
+    printf("     학번   : ");
 
     // 제대로 된 학번이 아닐때 예외처리
     scanf("%s", scanNum);
@@ -143,14 +112,14 @@ void init() {
     if (idx == 0)return;
 
 
-    if (studentsInfo[idx].num != 0)
+    if (studentsInfo[idx].num != 0) //존재하는 번호일때
     {
-        printf("이미 있음 ㅋㅋㄹㅃㅃ\n");
+        printf("\n   ERROR : 이미 존재하는 번호입니다.\n");
         return;
     }
-    if (strcmp(studentsInfo[idx].name, "전학") == 0)
+    if (strcmp(studentsInfo[idx].name, "전학") == 0)  //이름에 전학으로 처리가 되어있을때
     {
-        printf("전학 갔음 ㅋㅋㄹㅃㅃ\n");
+        printf("\n   ERROR : 전학 처리된 번호입니다.\n");
         return;
     }
 
@@ -158,21 +127,21 @@ void init() {
     studentsInfo[idx].num = atoi(scanNum);
 
 
-    printf("    이름   : ");
+    printf("     이름   : ");
     scanf("%s", studentsInfo[idx].name);
 
-    printf("    주소   : ");
+    printf("     주소   : ");
     scanf("%s", studentsInfo[idx].address);
 
-    printf("  전화번호 : ");
+    printf("   전화번호 : ");
     scanf("%s", studentsInfo[idx].phone);
 
-    while (1) {
-        printf("  국어점수 : ");
+    while (1) { //국어점수가 제대로된 갑이 입력될때까지 반복
+        printf("   국어점수 : ");
         scanf("%d", &studentsInfo[idx].koScore);
-        if (studentsInfo[idx].koScore > 100 || studentsInfo[idx].koScore < 0)
+        if (studentsInfo[idx].koScore > 100 || studentsInfo[idx].koScore < 0)    //100초과 0미만일때 ERROR
         {
-            printf("잘못 입력하셨습니다. 다시 입력하셈\n");
+            printf("\n   ERROR : 잘못된 입력입니다. 다시입력하세요.\n");
             continue;
         }
         else {
@@ -180,19 +149,18 @@ void init() {
         }
     }
 
-    while (1) {
-        printf("  자구점수 : ");
+    while (1) { //자구점수가 제대로 된 값이 입력될때까지 반복
+        printf("   자구점수 : ");
         scanf("%d", &studentsInfo[idx].dsScore);
-        if (studentsInfo[idx].dsScore > 100 || studentsInfo[idx].dsScore < 0)
+        if (studentsInfo[idx].dsScore > 100 || studentsInfo[idx].dsScore < 0)   //100초과 0미만일때 ERROR
         {
-            printf("잘못 입력하셨습니다. 다시 입력하셈\n");
+            printf("\n   ERROR : 잘못된 입력입니다. 다시입력하세요.\n");
             continue;
         }
         else {
             break;
         }
     }
-    printf("\n\n\n\n\n");
 
     if (studentsInfo[hiDSScore].dsScore < studentsInfo[idx].dsScore)
     {
@@ -202,119 +170,136 @@ void init() {
     {
         hiKoScore = idx;
     }
-
+    system("cls");
     return;
 }
 
-void del() {
-    char scanNum[5];
+void del() {        //학생의 전학처리를 담당하는 함수
+    char scanNum[5];    //입력값을 저장하는 5크기의 배열
     char scanchar;
     char idx;
-    printf("  ----------- 학생 전학 -----------\n");
-    printf("    학번   : ");
+    printf("   ----------- 학생 전학 -----------\n");
+    printf("     학번   : ");
 
     scanf("%s", scanNum);
     idx = studentNumberParsing(scanNum);
-    if (idx == 0)return;
+    if (idx == 0)return;          //반환값이 0일때(파싱과정에서 ERROR가 발생) 리턴
 
-    if (strcmp(studentsInfo[idx].name, "전학") == 0)
+    if (strcmp(studentsInfo[idx].name, "전학") == 0)  //이미 전학처리가 되었는가를 판단
     {
-        printf("이미 전학 갔음 ㅋㅋㄹㅃㅃ\n");
+        printf("\n   ERROR : 이미 전학처리된 학생입니다.\n");
         return;
     }
-    if (studentsInfo[idx].num == 0)
+    if (studentsInfo[idx].num == 0)                   //존재하는 학생인지를 판단
     {
-        printf("존재한적이 없음 ㅋㅋㄹㅃㅃ\n");
+        printf("\n   ERROR : 존재하지 않는 학생입니다.\n");
         return;
     }
 
     studentsInfo[idx].num = atoi(scanNum);
 
-    printf("  %d %s 이 전학처리 됩니다.\n",studentsInfo[idx].num, studentsInfo[idx].name);
+    printf("\n   %d %s 이 전학처리 됩니다.\n",studentsInfo[idx].num, studentsInfo[idx].name);   //전학처리됨을 출력
 
+    //전학처리되기로한 학생의 정보에대한 값을 모두 NULL값을 주고 이름 위치에 '전학'이라고 저장
     studentsInfo[idx].name[0] = '\0';
     strcat(studentsInfo[idx].name, "전학");
+    studentsInfo[idx].address[0] = '\0';
     studentsInfo[idx].dsScore = NULL;
     studentsInfo[idx].koScore = NULL;
     studentsInfo[idx].num = NULL;
     studentsInfo[idx].phone[0] = '\0';
-    printf("\n\n\n\n\n");
 
+    //고득점자를 지정하는 프로그램을 돌려 전학생의 점수가 나오지 안도록 처리함
     if (hiDSScore == idx)
     {
-        SelectTopDSScore();
+        SelectTopDSScore(); 
     }
     if (hiKoScore == idx)
     {
         SelectTopKoScore();
     }
-
+    system("cls");
     return;
 }
 
-void search()
+void search()       //학생검색을 위한 함수
 {
-    char scanNum[5];
-    int idx;
-    printf("  ------------학생 찾기------------\n");
+    char scanNum[5];    //입력값을 저장할 배열
+    int idx;            //반환값을 저장할 배열
+    char end_scan;
+
+    printf("\n   ------------학생 찾기------------\n"); 
     printf("  학번 입력 : ");
     scanf("%s", scanNum);
-    printf("\n\n");
-    idx = studentNumberParsing(scanNum);
+    idx = studentNumberParsing(scanNum);    
     if (idx == 0) return;
 
 
-    if (studentsInfo[idx].num != 0)
+    if (studentsInfo[idx].num == 0)
     {
-        printf("존재하지 않는 학생은 못찾아연\n");
+        printf("\n   ERROR : 존재하지 않는 학생입니다.\n");
         return;
     }
+     printf("\n   ------------검색 목록------------\n");
 
-    printf("  ------------검색 목록------------\n");
+    printf("     학번   : %d\n", studentsInfo[idx].num);
 
-    printf("    학번   : %d\n", studentsInfo[idx].num);
+    printf("     이름   : %s\n", studentsInfo[idx].name);
 
-    printf("    이름   : %s\n", studentsInfo[idx].name);
+    printf("     주소   : %s\n", studentsInfo[idx].address);
 
-    printf("    주소   : %s\n", studentsInfo[idx].address);
+    printf("   전화번호 : %s\n", studentsInfo[idx].phone);
 
-    printf("  전화번호 : %s\n", studentsInfo[idx].phone);
+    printf("   국어점수 : %d\n", studentsInfo[idx].koScore);
 
-    printf("  국어점수 : %d\n", studentsInfo[idx].koScore);
+    printf("   자구점수 : %d\n", studentsInfo[idx].dsScore);
 
-    printf("  자구점수 : %d\n", studentsInfo[idx].dsScore);
+    printf("\n   확인되었습니까?(Y) : ");
+    do {
+        scanf("%c", &end_scan);
+    } while (end_scan == 10);
+    if (end_scan == 'Y' || end_scan == 'y')   //만약 입력값이 y라면
+        system("cls");
+    else {
+        printf("\n   종료를 하실려면 Y를 눌러주세요.\n");
+
+    }
 }
 
+//성적 고득점자를 출력하는 코드
 void topScore() {
     if (hiDSScore == 0 || hiKoScore == 0)
     {
-        printf("성적 우수자가 존재하지 않습니다.\n\n");
+        printf("\n   ERROR : 성적 우수자가 존재하지 않습니다.\n\n");
         return;
     }
 
-    printf("  +-------- 성적 우수자 ----------+\n");
-    printf("    국어 : %d %s (%d)\n", studentsInfo[hiKoScore].num, studentsInfo[hiKoScore].name, studentsInfo[hiKoScore].koScore);
-    printf("    자구 : %d %s (%d)\n", studentsInfo[hiDSScore].num, studentsInfo[hiDSScore].name, studentsInfo[hiDSScore].dsScore);
-    printf("\n\n\n\n\n");
+    printf("\n   +-------- 성적 우수자 ----------+\n");
+    printf("     국어 : %d %s (%d)\n", studentsInfo[hiKoScore].num, studentsInfo[hiKoScore].name, studentsInfo[hiKoScore].koScore);
+    printf("     자구 : %d %s (%d)\n", studentsInfo[hiDSScore].num, studentsInfo[hiDSScore].name, studentsInfo[hiDSScore].dsScore);
+    system("cls");
+
 }
 
+//자구점수 고득점자를 판단하는 코드
 void SelectTopDSScore()
 {
     int maxDSScore = INT_MIN;
     hiDSScore = 0;
-    for (int i = 0; i < STUDENT_CNT; i++)
+    for (int i = 0; i < STUDENT_CNT; i++)   //학생수만큼 for문을 돌린다.
     {
-        if (studentsInfo[i].num != 0)
+        if (studentsInfo[i].num != 0)       //학생의 정보가 있는가를 판단하여
         {
-            if (maxDSScore < studentsInfo[i].dsScore)
+            if (maxDSScore < studentsInfo[i].dsScore)   //있다면 최고득점자값보다 학생의 점수가 높은지 판단
             {
-                maxDSScore = studentsInfo[i].dsScore;
+                maxDSScore = studentsInfo[i].dsScore;   //이후 최고득점자의 값에 저장
                 hiDSScore = i;
             }
         }
     }
 }
 
+//국어점수고득점자를 판단하는 코드(위의 코드와 상세설명이 같으므로 생략)
 void SelectTopKoScore()
 {
     int maxKoScore = INT_MIN;
@@ -333,29 +318,29 @@ void SelectTopKoScore()
 }
 
 
-
+//입력받은 값(scanNum)을 각 자리수로나누어 에러를 감지하고 정수형으로 반환
 int studentNumberParsing(char* scanNum)
 {
     int returnNum = 0;
-             if (strlen(scanNum) != 4)
+     if (strlen(scanNum) != 4)  //4자리수가 아닐때 ERROR
     {
-        printf("TIP! : 알고계셨나요? 학번은 4글자입니다.\n");
+        printf("\n   ERROR : 4글자로 숫자를 구성하여 입력하세요.\n");
         return 0;
     }
-    if (scanNum[0] > '3' || scanNum[0] < '1') {
-        printf("TIP! : 알고계셨나요? 학년은 1학년부터 3학년까지만 있답니다.\n"); 
+    if (scanNum[0] > '3' || scanNum[0] == '0') {    //학년 범위를 벗어났을때 ERROR
+        printf("\n   ERROR : 학년(1 ~ 3사이의 수를 입력하세요.)\n"); 
         return 0;
     }
-    if (scanNum[1] > '3' || scanNum[1] < '1') {
-        printf("TIP! : 알고계셨나요? 반은 1반부터 3반까지만 있답니다.\n");
+    if (scanNum[1] > '3' || scanNum[1] == '0') {    //학반 범위를 벗어났을때 ERROR
+        printf("\n   ERROR : 학반(1 ~ 3사이의 수를 입력하세요.)\n");
         return 0;
     }
-    if (scanNum[2] > '2' || scanNum[2] < '0' || scanNum[3] > '9' || scanNum[3] < '0' || (scanNum[2] == '2' && scanNum[3] != '0')) {
-        printf("TIP! : 알고계셨나요? 번호는 1번부터 20번까지만 있답니다.\n");
+    if ((scanNum[2]=='0' && scanNum[3]=='0') || (scanNum[2] >= '2' && scanNum[3] > '0')) {  //학번 범위를 벗어났을때 ERROR
+        printf("\n   ERROR : 학번(01 ~ 20사이의 수를 입력하세요.)\n");
         return 0;
     }
 
-
+    //배열로 나누어 받은 값을 학년에 60을 곱하고 반에 20을 곱하고 번호를 이용하여 학생순번을 0부터 180까지 나열
     returnNum += (scanNum[0] - '0' - 1) * 60;
     returnNum += (scanNum[1] - '0' - 1) * 20;
     returnNum += (scanNum[2] - '0') * 10;
